96e543ca78650042d53e1216ae6735fe16b7370e
김현우 <hyeonwoo@hyeonwoo-mac-air.local>
2023-09-10 17:09:21 +0900

    express 세팅

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("hello");
+});
+
+app.listen("3000", () => {
+  console.log("Listen at 3000...");
+});
